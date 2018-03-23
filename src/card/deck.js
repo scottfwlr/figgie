d4ac98b8a419eae74c28c47c454d6703c3a5b4ff
scottfwlr <this.specific.scott@gmail.com>
2018-03-23 18:05:35 -0400

    strange experimentation

---
+++ src/card/deck.js
@@ -0,0 +1,43 @@
+import { buildElem, div } from 'elementbuilder';
+
+const cardFront = buildElem('div', 'card-front');
+const cardBack = buildElem('div', 'card-back');
+const innerCard = buildElem('div', 'card', { 
+  cardFront, 
+  cardBack 
+})
+const card = buildElem('div', 'card-container', {
+  innerCard
+})
+
+export const baseCard = () => card.cloneNode(true);
+
+
+const otherCard = buildElem('div', 'card-container', {
+  otherInnerCard: buildElem('div', 'card', {
+    cardFront: buildElem('div', 'card-front'),
+    cardBack: buildElem('div', 'card-back')
+  })
+})
+
+export const otherBaseCard = () => otherCard.cloneNode(true);
+
+
+const thirdCard = div('card-container', {
+  innerCard: div('card', {
+    cardFront: div('card-front'),
+    cardBack: div('card-back')
+  })
+})
+
+export const thirdBaseCard = () => thirdCard.cloneNode(true);
+
+
+export const cardMaker = ({ card="", front="", back="" } = {}) => (
+  div('card-container', {
+    innerCard: div(`card ${card}`, {
+      cardFront: div(`card-front ${front}`),
+      cardBack: div(`card-back ${back}`)
+    })
+  })
+)
--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import header from 'css/header';
 import page from 'css/page';
 import card from 'css/card';
 
+import { cardMaker } from 'deck';
+
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.getElementById('header');
   const intro = document.getElementById('intro');
   const game = document.getElementById('game');
   const postgame = document.getElementById('postgame');
+
   game.classList.add('visible');
+
+  const toggle = property => element => {
+    element.classList.contains(property) ?
+    element.classList.remove(property) :
+    element.classList.add(property)
+  }
+
+  const cards = Array.from(document.getElementsByClassName('card'));
+  const flip = () => cards.forEach(toggle('flipped'));
+  window.flip = flip;
+  window.cardMaker = cardMaker;
+
 })
