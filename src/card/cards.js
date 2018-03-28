dbd0df9f01adc6f9a904a97510fc2f84bf7fd156
scottfwlr <this.specific.scott@gmail.com>
2018-03-27 22:31:24 -0400

    amazing

--- src/card/cards.js
+++ src/card/cards.js
@@ -1,3 +1,5 @@
+
+
 function newDeck() {
   const suits = ['diams', 'hearts', 'spades', 'clubs'];
   const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
@@ -36,29 +38,39 @@ const setup = () => {
   return {
     hand: newHand(),
     deck: newDeck(),
-    cardState: entity => ({ entity, x: null, y: null, absX: null, absY: null }),
+    cardState: entity => ({ entity, x: null, y: null, absX: null, absY: null, offset: null }),
     movePair: (entity, toNode) => ({ entity, toNode }),
     rawMove: ({ entity, toNode }) => toNode.appendChild(entity),
-    
+
     move: (pairs) => {
-      const cards = Object.values(Cards.deck).map(Cards.cardState);
 
       // the FLIP animation technique:
+      // https://medium.com/developers-writing/animating-the-unanimatable-1346a5aab3cd
+
+
+      // get old location (of all cards)
+
+      const cards = Object.values(Cards.deck).map(Cards.cardState);
+      
+
       // within a single animation frame...
       window.requestAnimationFrame(() => {
-        // get old location (of all cards)
+ 
         cards.forEach(card => {
           let { x, y } = card.entity.getBoundingClientRect();
+          x = (x === 0 ? -63 : x);
+          y = (y === 0 ? -88 : y);
           card.x = x;
           card.y = y;
         });
-
         // make the DOM changes (of just the moved cards)
         pairs.forEach(pair => Cards.rawMove(pair));
 
         // get the new location (of all cards)
         cards.forEach(card => {
           let { x, y } = card.entity.getBoundingClientRect();
+          x = (x === 0 ? -63 : x);
+          y = (y === 0 ? -88 : y);
           card.x = card.x - x;
           card.absX = Math.abs(x);
           card.y = card.y - y;
@@ -71,28 +83,34 @@ const setup = () => {
         // find out how they changed
         let bigX = 0;
         let bigY = 0;
+
         moved.forEach(({ absX, absY }) => {
           if (absX > bigX) bigX = absX;
           if (absY > bigY) bigY = absY;
         });
 
+        // calculate some offsets to normalise card speed
+        moved.forEach(card => card.offset = Math.max((1 - card.absX/bigX), (1 - card.absY/bigY), 0));
+
         moved.forEach(card => {
-          // calculate some offsets to normalise card speed
-          const offset = Math.max((1 - card.absX/bigX), (1 - card.absY/bigY), 0);
           // style the cards to look like they haven't changed
+          const tS = `translate3d(${card.x}px, ${card.y}px, 0)`
+          const tE = 'translate3d(0,0,0)'
           const keyframes = [
-            {transform: `translate3d(${card.x}px, ${card.y}px, 0)`},
-            {transform: `translate3d(${card.x}px, ${card.y}px, 0)`, offset},
-            {transform: 'translate3d(0,0,0)'}
+            {transform: tS},
+            {transform: tS, offset: card.offset},
+            {transform: tE}
           ];
           // animate their transition to the new location
           card.entity.animate(keyframes, { 
-          // introduce some organic randomness
-            duration: Math.max(50, ( (1000-offset*1000) + (100 - (Math.random()*200)) )) 
+            easing: 'ease',
+          // introduce some organic-looking randomness
+            duration: Math.max(250, (1000-(card.offset*500))) + (50 - (Math.random()*100))
           });
         });
       });
     },
+
     pickupCards: (...names) => {
       Cards.move(names.map(name => {
         return Cards.movePair(Cards.deck[name], Cards.hand[name.split('-')[2]]);
--- src/index.js
+++ src/index.js
@@ -14,5 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   game.classList.add('visible');
 
+  window.setTimeout(() => {
+    Cards.pickupCards('4-of-clubs', '5-of-clubs', '6-of-clubs', '7-of-clubs')
+  }, 1000);
+  
+  window.setTimeout(() => {
+    Cards.pickupCards('4-of-diams', '5-of-diams', '6-of-diams', '7-of-diams')
+  }, 3000);
+  
+  window.setTimeout(() => {
+    Cards.pickupCards('4-of-spades', '5-of-spades', '6-of-spades', '7-of-spades')
+  }, 5000);
+
+  window.setTimeout(() => {
+    Cards.pickupCards('4-of-hearts', '5-of-hearts', '6-of-hearts', '7-of-hearts')
+  }, 7000);
+  
+  window.setTimeout(() => {
+    Cards.move(Array.from(document.querySelectorAll('.suit-container .card-container')).map(function(el, i) {
+      return Cards.movePair(el, Cards.hand[Object.keys(Cards.hand)[i%4]])
+    }));
+  }, 10000);
+
+
+
 
 })
